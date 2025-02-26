78b00474408ae5df8fed5ab8f93819e579b8c7d2
xialuna <xianhuicheng01@gmail.com>
2025-02-26 19:20:35 +0800

    chore: configure design system

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,31 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        brand: {
+          primary: '#F6C900',
+          'primary-hover': '#FADF66'
+        },
+        neutral: {
+          50: '#FFFFFF',
+          100: '#D2D2D2',
+          200: '#A6A5A5',
+          300: '#797979',
+          400: '#201F1F',
+          500: '#1A1919',
+          600: '#131313',
+          700: '#0D0C0C',
+          800: '#060606'
+        }
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
